6c6d3f4b76f8f77ec372ac49a8742e49ee13b4ec
Otubusin Ademuyiwa <53128146+tay4real@users.noreply.github.com>
2020-12-08 17:24:25 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const studentsRoutes = require("./students")
-
+const cors = require("cors")
 const server = express()
 
 const port = 3001
+
+
+
 
 server.use(express.json()) // must be specified 
 
 server.use("/students", studentsRoutes)
 
+server.use(cors())
 server.listen(port, () => {
     console.log("Server is running on port:", port)
 })
